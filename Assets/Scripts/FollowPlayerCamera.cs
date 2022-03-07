ced17569c79f473da94cd8bd94473ad4a4444720
Miika Savela <miika.a.savela@gmail.com>
2022-03-07 20:16:42 +0200

    Adjust camera position to be over player and update Player prefab to display player rotation better

---
+++ Assets/Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour
+{
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = player.transform.position;
+    }
+}
