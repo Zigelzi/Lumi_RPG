4399e37ad2852958ddfb857f5862f6c99c00c006
Miika Savela <miika.a.savela@gmail.com>
2022-03-14 19:25:31 +0200

    Add chase range check to enemies

---
+++ Assets/Scripts/Control/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 100f)] float chaseDistance = 5f;
+
+        GameObject player;
+
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        void Update()
+        {
+            if (IsPlayerInChaseRange())
+            {
+                Chase();
+            }
+        }
+
+        bool IsPlayerInChaseRange()
+        {
+            if (player == null) return false;
+
+            return Vector3.Distance(transform.position, player.transform.position) <= chaseDistance;
+        }
+
+        void Chase()
+        {
+            Debug.Log($"{gameObject.name} is chasing player!");
+        }
+    }
+}
