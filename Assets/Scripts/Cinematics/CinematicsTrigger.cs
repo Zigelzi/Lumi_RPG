688a21bc7376820959513a7e543ed83677efc015
Miika Savela <miika.a.savela@gmail.com>
2022-03-26 11:49:41 +0200

    Add initial reveal cinematic

---
+++ Assets/Scripts/Cinematics/CinematicsTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsTrigger : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            PlayableDirector director = GetComponent<PlayableDirector>();
+
+            if (other.gameObject.tag == "Player")
+            {
+                director.Play();
+            }
+            
+        }
+    }
+}
+
