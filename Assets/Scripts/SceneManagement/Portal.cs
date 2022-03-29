dc08c3ee332bbc294a4c875c577acb854ab0d0b5
Miika Savela <miika.a.savela@gmail.com>
2022-03-29 19:51:12 +0300

    Update the hill scene terrain and add portals to move between scenes

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 10)] int destinationSceneIndex = 0;
+        [SerializeField] Transform spawnPoint;
+        void Awake()
+        {
+              
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                StartCoroutine(TransitionToScene());
+            }
+        }
+
+        IEnumerator TransitionToScene()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(destinationSceneIndex);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayerLocation(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        Portal GetOtherPortal()
+        {
+            Portal[] portals = FindObjectsOfType<Portal>();
+
+            foreach (Portal portal in portals)
+            {
+                if (portal == this)
+                {
+                    continue;
+                }
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        void UpdatePlayerLocation(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            player.GetComponent<NavMeshAgent> ().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+    }
+}
+
