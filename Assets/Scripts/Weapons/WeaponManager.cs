648e4089078db3caa868428261ac9911deb52ece
Miika Savela <miika.a.savela@gmail.com>
2022-04-11 08:22:24 +0300

    Add spawning sword to player

---
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    public class WeaponManager : MonoBehaviour
+    {
+        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Transform weaponHoldingLocation = null;
+        // Start is called before the first frame update
+        void Start()
+        {
+            SpawnWeapon();
+        }
+
+        void SpawnWeapon()
+        {
+            if (CanSpawnWeapon())
+            {
+                Instantiate(weaponPrefab, weaponHoldingLocation);
+            }
+            
+        }
+
+        bool CanSpawnWeapon()
+        {
+            if (weaponPrefab != null && weaponHoldingLocation != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
