ce55462e8cadf086bdb6d32f62d735d51600abe3
Miika Savela <miika.a.savela@gmail.com>
2022-05-12 08:36:16 +0300

    Add health pickup and add sword to pickup and adjust first enemies

--- Assets/Scripts/Combat/ConsumablePickup.cs
+++ Assets/Scripts/Combat/ConsumablePickup.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Core;
 
-public class ConsumablePickup : MonoBehaviour
+namespace RPG.Combat
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ConsumablePickup : MonoBehaviour
     {
-        
-    }
+        [SerializeField] ConsumableType consumableType = ConsumableType.Health;
+        [SerializeField][Range(0, 100)] int amount = 20;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        enum ConsumableType
+        {
+            Health
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                ConsumePickup(other);
+            }    
+        }
+
+        void ConsumePickup(Collider playerCollider)
+        {
+            if (consumableType == ConsumableType.Health)
+            {
+                Health playerHealth = playerCollider.GetComponent<Health>();
+
+                if (playerHealth == null) return;
+
+                Heal(playerHealth);
+            }
+        }
+
+        void Heal(Health playerHealth)
+        {
+            bool isHealed = playerHealth.AddHealth(amount);
+
+            if (isHealed)
+            {
+                Destroy(gameObject);
+            }
+        } 
     }
 }
+
