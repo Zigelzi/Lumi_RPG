ade284f83b18fcac0942021037baad117fb3ed00
Miika Savela <miika.a.savela@gmail.com>
2022-05-23 07:56:45 +0300

    Add player info UI and displaying the current health

---
+++ Assets/Scripts/UI/PlayerInformation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+using RPG.Attributes;
+
+public class PlayerInformation : MonoBehaviour
+{
+    [SerializeField] TMP_Text healthValue;
+
+    GameObject player;
+    Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<Health>();
+        SetHealthValue();
+
+        playerHealth.OnHealthChange += HandleHealthUpdate;
+    }
+
+    void OnDestroy()
+    {
+        playerHealth.OnHealthChange -= HandleHealthUpdate;
+    }
+
+    void SetHealthValue()
+    {
+        if (healthValue == null) return;
+
+        string healthText = $"{playerHealth.CurrentHealth} / {playerHealth.MaxHealth}";
+        healthValue.text = healthText;
+    }
+    void SetHealthValue(float value)
+    {
+        if (healthValue == null) return;
+
+        string healthText = $"{value} / {playerHealth.MaxHealth}";
+        healthValue.text = healthText;
+    }
+
+    void HandleHealthUpdate(float newHealth)
+    {
+        SetHealthValue(newHealth);
+    }
+    
+}
