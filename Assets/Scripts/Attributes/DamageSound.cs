26d7a7cd49dc523ca05056f4a11b0b51b87bb19c
Miika Savela <miika.a.savela@gmail.com>
2022-07-18 11:07:50 +0300

    Add playing SFX when taking damage and randomizing it's pitch

---
+++ Assets/Scripts/Attributes/DamageSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.SFX;
+
+namespace RPG.Attributes
+{
+    public class DamageSound : MonoBehaviour
+    {
+        [SerializeField] AudioClip damageSFX;
+        AudioSource audioSource;
+        Health health;
+        SoundRandomizer soundRandomiser;
+
+        void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            health = GetComponentInParent<Health>();
+            soundRandomiser = GetComponent<SoundRandomizer>();
+        }
+
+        void OnEnable()
+        {
+            health.onDamageTaken += HandleDamageTaken;    
+        }
+
+        void OnDisable()
+        {
+            health.onDamageTaken -= HandleDamageTaken;    
+        }
+
+        void HandleDamageTaken(float amount)
+        {
+            if (audioSource == null || damageSFX == null) return;
+
+            soundRandomiser.RandomisePitch(audioSource);
+            audioSource.clip = damageSFX;
+            audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/SFX/SoundRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SFX
+{
+    public class SoundRandomizer : MonoBehaviour
+    {
+        [SerializeField][Range(-3, 3)] float pitchMin = 0;
+        [SerializeField][Range(-3, 3)] float pitchMax = 1f;
+
+        public void RandomisePitch(AudioSource audioSource)
+        {
+            if (audioSource == null) return;
+
+            if (pitchMin < pitchMax)
+            {
+                float pitch = Random.Range(pitchMin, pitchMax);
+                audioSource.pitch = pitch;
+            }
+        }
+    }
+}
