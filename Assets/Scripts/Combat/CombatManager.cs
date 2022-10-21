f1e7c94b77c5346e5f9efe8818a722e0bce0ee15
Miika Savela <miika.a.savela@gmail.com>
2022-10-21 08:36:28 +0300

    Add player set to combat when taking damage

---
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,58 @@
+﻿using RPG.Attributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class CombatManager : MonoBehaviour
+    {
+        [SerializeField] float combatDuration = 3f;
+        [SerializeField] float durationInCombatRemaining = 0;
+
+        Health health;
+
+        void Awake()
+        {
+            health = GetComponent<Health>();
+        }
+
+        void OnEnable()
+        {
+            health.onDamageTaken.AddListener(HandleDamageTaken);
+        }
+
+        void Update()
+        {
+            ReduceCombatTimer();
+        }
+
+        void OnDisable()
+        {
+            health.onDamageTaken.RemoveListener(HandleDamageTaken);
+        }
+
+        public bool IsInCombat()
+        {
+            return durationInCombatRemaining > 0;
+        }
+
+        void HandleDamageTaken(float amount)
+        {
+            durationInCombatRemaining = combatDuration;
+        }
+
+        void ReduceCombatTimer()
+        {
+            if (durationInCombatRemaining > 0)
+            {
+                durationInCombatRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                durationInCombatRemaining = 0;
+            }
+        }
+    }
+
+}
