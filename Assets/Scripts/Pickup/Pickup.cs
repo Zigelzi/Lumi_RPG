226e76054859e7ebe7ea85f8ec357cae2e2bc205
Miika Savela <miika.a.savela@gmail.com>
2022-10-22 13:20:36 +0300

    Add updating the cursor type and handling the raycast to new flexible pickups

--- Assets/Scripts/Pickup/Pickup.cs
+++ Assets/Scripts/Pickup/Pickup.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using RPG.Control;
+using RPG.Movement;
+
 namespace RPG.Pickup
 {
-    public class Pickup : MonoBehaviour
+    public class Pickup : MonoBehaviour, IRaycastable
     {
         [SerializeField] PickupConfig pickUpConfig;
+
+        public bool HandleRaycast(PlayerController player, RaycastHit hit)
+        {
+            UnitMovement movement = player.GetComponent<UnitMovement>();
+            if (movement.CanMoveTo(hit.point))
+            {
+                player.TryStartMoveAction(hit.point);
+                return true;
+            }
+
+            return false;
+        }
+
+        public CursorType GetCursorType()
+        {
+            return CursorType.Interactable;
+        }
+
+
         private void OnTriggerEnter(Collider other)
         {
             if (pickUpConfig == null) return;
