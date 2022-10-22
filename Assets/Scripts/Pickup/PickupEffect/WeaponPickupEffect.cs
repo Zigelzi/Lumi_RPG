ea80a37882445ea4021f4f621e642d27cb9123c9
Miika Savela <miika.a.savela@gmail.com>
2022-10-22 14:39:40 +0300

    Add general weapon pickup effect with sword and bow variants

---
+++ Assets/Scripts/Pickup/PickupEffect/WeaponPickupEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.Combat;
+
+namespace RPG.Pickup
+{
+    [CreateAssetMenu(fileName = "PickupEffect_Weapon_", menuName = "Pickups/Pickup effects/Weapon", order = 0)]
+    public class WeaponPickupEffect : PickupEffectStrategy
+    {
+        [SerializeField] WeaponConfig weaponConfig;
+
+        public override bool GrantEffect(PickupData data)
+        {
+            if (weaponConfig == null) return false;
+
+            if (data.User.TryGetComponent<WeaponManager>(out WeaponManager weaponManager))
+            {
+                weaponManager.EquipWeapon(weaponConfig);
+                return true;
+            }
+            return false;
+        }
+    }
+}
