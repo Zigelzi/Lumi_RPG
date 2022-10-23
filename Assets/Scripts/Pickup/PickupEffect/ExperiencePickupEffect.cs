176598ea25b97eb731c41cb5fa918c979874f1cb
Miika Savela <miika.a.savela@gmail.com>
2022-10-23 10:39:47 +0300

    Add experience pickup and add enemies and pickups to sandbox level

---
+++ Assets/Scripts/Pickup/PickupEffect/ExperiencePickupEffect.cs
@@ -0,0 +1,23 @@
+﻿using RPG.Stats;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Pickup
+{
+    [CreateAssetMenu(fileName = "PickupEffect_Experience_", menuName = "Pickups/Pickup effects/Experience", order = 0)]
+    public class ExperiencePickupEffect : PickupEffectStrategy
+    {
+        [SerializeField] float experienceAmount = 20f;
+
+        public override bool GrantEffect(PickupData data)
+        {
+            if (data.User.TryGetComponent<Experience>(out Experience experience))
+            {
+                experience.AddExperience(experienceAmount);
+                return true;
+            }
+            return false;
+        }
+    }
+}
