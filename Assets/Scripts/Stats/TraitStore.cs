f76613fad46885c7fc85a1e53a921e20ce478c11
Miika Savela <miika.a.savela@gmail.com>
2023-05-11 20:00:14 +0300

    Add increasing player stats based on assigned traits

--- Assets/Scripts/Stats/TraitStore.cs
+++ Assets/Scripts/Stats/TraitStore.cs
@@ -10,11 +10,24 @@ namespace RPG.Stats
     {
         // TODO: Player can increase their stats when traits are committed
         [SerializeField] int _unassignedPoints = 0;
+        [SerializeField] TraitBonus[] _bonusConfig;
 
         Dictionary<Trait, int> _assignedTraits = new Dictionary<Trait, int>();
         Dictionary<Trait, int> _stagedTraits = new Dictionary<Trait, int>();
 
+        Dictionary<Stat, Dictionary<Trait, float>> _additiveBonusCache;
+        Dictionary<Stat, Dictionary<Trait, float>> _percentageBonusCache;
+
         BaseStats _baseStats;
+
+        [System.Serializable]
+        class TraitBonus
+        {
+            public Trait trait = Trait.Intelligence;
+            public Stat stat = Stat.Attunement;
+            public float additiveBonusPerPoint = 0;
+            public float percentageBonusPerPoint = 0;
+        }
 
         public UnityEvent onTraitAssigned;
 
@@ -23,6 +36,7 @@ namespace RPG.Stats
         void Awake()
         {
             _baseStats = GetComponent<BaseStats>();
+            BuildBonusCaches();
         }
 
         void OnEnable()
@@ -81,7 +95,8 @@ namespace RPG.Stats
 
         public void Commit()
         {
-            foreach(Trait trait in _stagedTraits.Keys)
+            // FIX: Bonuses are not applied when committing them
+            foreach (Trait trait in _stagedTraits.Keys)
             {
                 _assignedTraits[trait] = GetProposedPoints(trait);
             }
@@ -92,12 +107,69 @@ namespace RPG.Stats
 
         public IEnumerable<float> GetAdditiveModifier(Stat stat)
         {
-            yield return 0;
+            if (!_additiveBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (Trait trait in _additiveBonusCache[stat].Keys)
+            {
+                float bonus = _additiveBonusCache[stat][trait];
+
+                Debug.Log($"Added {bonus} points to {trait} for {stat}");
+
+                yield return bonus * GetPoints(trait);
+            }
         }
 
         public IEnumerable<float> GetPercentageModifier(Stat stat)
         {
-            yield return 0;
+            if (!_percentageBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (Trait trait in _percentageBonusCache[stat].Keys)
+            {
+                float bonus = _percentageBonusCache[stat][trait];
+
+                yield return bonus * GetPoints(trait);
+            }
+        }
+
+        void BuildBonusCaches()
+        {
+            _additiveBonusCache = new Dictionary<Stat, Dictionary<Trait, float>>();
+            _percentageBonusCache = new Dictionary<Stat, Dictionary<Trait, float>>();
+
+            foreach (TraitBonus bonus in _bonusConfig)
+            {
+
+                if (!_additiveBonusCache.ContainsKey(bonus.stat))
+                {
+                    Dictionary<Trait, float> additiveBonus = BuildAdditiveBonus(bonus);
+                    _additiveBonusCache.Add(bonus.stat, additiveBonus);
+
+                }
+                
+                if (!_percentageBonusCache.ContainsKey(bonus.stat))
+                {
+                    Dictionary<Trait, float> percentageBonus = BuildPercentageBonus(bonus);
+                    _percentageBonusCache.Add(bonus.stat, percentageBonus);
+                }
+
+            }
+
+        }
+
+        Dictionary<Trait, float> BuildAdditiveBonus(TraitBonus bonus)
+        {
+            Dictionary<Trait, float> additiveBonus = new Dictionary<Trait, float>();
+            additiveBonus.Add(bonus.trait, bonus.additiveBonusPerPoint);
+
+            return additiveBonus;
+        }
+
+        Dictionary<Trait, float> BuildPercentageBonus(TraitBonus bonus)
+        {
+            Dictionary<Trait, float> percentageBonus = new Dictionary<Trait, float>();
+            percentageBonus.Add(bonus.trait, bonus.percentageBonusPerPoint);
+
+            return percentageBonus;
         }
 
         void HandleLevelChange(int newLevel)
